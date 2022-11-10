e945ab56268e0464d7471b95a3613b44dd523c72
lisstem <mail@lisstem.net>
2022-11-10 20:12:27 +0100

    Can setup/teardown table aliases.

--- lib/mail_tools/command/setup.rb
+++ lib/mail_tools/command/setup.rb
@@ -9,34 +9,39 @@ module MailTools
         domains
         addresses
         users
+        aliases
       end
 
-      def domains
-        result = db.exec("CREATE TABLE IF NOT EXISTS domains ("\
-                         "id serial PRIMARY KEY NOT NULL,"\
-                         "name text UNIQUE NOT NULL"\
-                         ");")
-        result.check
-      end
+      # rubocop:disable Layout/HashAlignment
+      QUERIES = { domains:   "CREATE TABLE IF NOT EXISTS domains ("\
+                              "id serial PRIMARY KEY NOT NULL,"\
+                              "name text UNIQUE NOT NULL"\
+                              ");",
+                  addresses:  "CREATE TABLE IF NOT EXISTS addresses ("\
+                              "id serial PRIMARY KEY NOT NULL, "\
+                              "name text NOT NULL, "\
+                              "domain_id int NOT NULL REFERENCES domains(id) ON DELETE CASCADE, "\
+                              "UNIQUE (name, domain_id)"\
+                              ");",
+                  users:      "CREATE TABLE IF NOT EXISTS users ("\
+                              "id serial PRIMARY KEY NOT NULL, "\
+                              "name text UNIQUE NOT NULL, "\
+                              "password text NOT NULL, "\
+                              "address_id int NOT NULL UNIQUE REFERENCES addresses(id) ON DELETE CASCADE"\
+                              ");",
+                  aliases:    "CREATE TABLE IF NOT EXISTS aliases ("\
+                             "id serial PRIMARY KEY NOT NULL, "\
+                             "source_id INT NOT NULL REFERENCES addresses(id) ON DELETE CASCADE,"\
+                             "destination_id INT NOT NULL REFERENCES addresses(id) ON DELETE CASCADE, "\
+                             "UNIQUE (source_id, destination_id)"\
+                             ");" }.freeze
+      # rubocop:enable Layout/HashAlignment
 
-      def addresses
-        result = db.exec("CREATE TABLE IF NOT EXISTS addresses ("\
-                         "id serial PRIMARY KEY NOT NULL, "\
-                         "name text NOT NULL, "\
-                         "domain_id int NOT NULL REFERENCES domains(id) ON DELETE CASCADE, "\
-                         "UNIQUE (name, domain_id)"\
-                         ");")
-        result.check
-      end
-
-      def users
-        result = db.exec("CREATE TABLE IF NOT EXISTS users ("\
-                         "id serial PRIMARY KEY NOT NULL,"\
-                         "name text UNIQUE NOT NULL,"\
-                         "password text NOT NULL,"\
-                         "address_id int NOT NULL UNIQUE REFERENCES addresses(id) ON DELETE CASCADE"\
-                          ");")
-        result.check
+      QUERIES.each_pair do |sym, query|
+        define_method sym do
+          result = db.exec(query)
+          result.check
+        end
       end
     end
   end
--- lib/mail_tools/command/teardown.rb
+++ lib/mail_tools/command/teardown.rb
@@ -6,14 +6,16 @@ module MailTools
   module Command
     class Teardown < Base
       def default
+        aliases
         users
         addresses
         domains
       end
       
-      %i[domains addresses users].each do |sym|
+      %i[domains addresses users aliases].each do |sym|
         define_method sym do
-          result = db.exec("DROP TABLE IF EXISTS #{sym.to_s};")
+          result = db.exec("DROP TABLE IF EXISTS #{sym};")
+          result.check
         end
       end
     end
